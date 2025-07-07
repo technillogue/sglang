ce836ff1db060da23d4b4430a85fdc842afcb4af
ybyang <ybyang7@iflytek.com>
2025-07-07 10:21:55 +0800

    add multi

--- python/sglang/srt/server_args.py
+++ python/sglang/srt/server_args.py
@@ -1757,10 +1757,7 @@ class PortArgs:
     # The ipc filename for detokenizer to receive inputs from scheduler (zmq)
     detokenizer_ipc_name: str
 
-    # The port for
-
-
-    initialization (torch.dist)
+    # The port for initialization (torch.dist)
     nccl_port: int
 
     # The ipc filename for rpc call between Engine and Scheduler
